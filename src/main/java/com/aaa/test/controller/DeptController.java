0f2a90388b600ed06c4ca3d5fb89c020c136f38c
MVP-xu <924578640@qq.com>
2018-11-29 09:32:07 +0800

    11.29提交测试

---
+++ src/main/java/com/aaa/test/controller/DeptController.java
@@ -0,0 +1,13 @@
+package com.aaa.test.controller;
+
+/**
+ * className:DeptController
+ * discription:
+ * author:MVP
+ * createTime:2018-11-29 09:09
+ */
+public class DeptController {
+    public static void main(String[] args) {
+        System.out.println(6666);
+    }
+}
